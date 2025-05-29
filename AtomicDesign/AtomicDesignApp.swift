38371a208af74b16ad5ee0b073a2f0adf2d508a7
jageenshukla <41036256+jageenshukla@users.noreply.github.com>
2025-05-29 15:29:32 +0900

    Initial Commit

---
+++ AtomicDesign/AtomicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomicDesignApp.swift
+//  AtomicDesign
+//
+//  Created by Shukla, Jageen | Jageen | ESDD on 2025/05/29.
+//
+
+import SwiftUI
+
+@main
+struct AtomicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AtomicDesign/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AtomicDesign
+//
+//  Created by Shukla, Jageen | Jageen | ESDD on 2025/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
