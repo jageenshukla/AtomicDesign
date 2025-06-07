c1ca688fd12ab86f61f0b1770726e233b07f12b5
jageenshukla <41036256+jageenshukla@users.noreply.github.com>
2025-06-07 14:52:14 +0900

    Remove debug statements

--- AtomicDesign/Components/Organisms/DynamicNavigationBar.swift
+++ AtomicDesign/Components/Organisms/DynamicNavigationBar.swift
@@ -26,10 +26,6 @@ struct DynamicNavigationBar: UIViewControllerRepresentable {
                 let appearance = UINavigationBarAppearance()
                 appearance.configureWithOpaqueBackground()
 
-                print("DynamicNavigationBar: Updating navigation bar appearance")
-                print("DynamicNavigationBar: Background color - \(self.backgroundColor)")
-                print("DynamicNavigationBar: Title color - \(self.titleColor)")
-
                 appearance.backgroundColor = self.backgroundColor
                 appearance.titleTextAttributes = [.foregroundColor: self.titleColor]
                 appearance.largeTitleTextAttributes = [.foregroundColor: self.titleColor]
@@ -40,12 +36,6 @@ struct DynamicNavigationBar: UIViewControllerRepresentable {
 
                 // Force navigation bar to refresh
                 navigationBar.layoutIfNeeded()
-
-                print("DynamicNavigationBar: Navigation bar appearance updated")
-                print("DynamicNavigationBar: Resolved backgroundColor - \(self.backgroundColor)")
-                print("DynamicNavigationBar: Resolved titleColor - \(self.titleColor)")
-            } else {
-                print("DynamicNavigationBar: Navigation bar not found")
             }
         }
     }
